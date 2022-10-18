a8683dc92d4d1f953d82380e632e36d5654c8d5f
Abinash Ram Sunthar <Abinash3366@gmail.com>
2022-10-18 18:52:08 +1100

    Added Exec Timeout of basic config & Password Encrpytion

--- dependencies/basic.py
+++ dependencies/basic.py
@@ -13,8 +13,18 @@ def Basic(Switch, Router):
     Conpass = input("What is the console password of the device? ")
     SSHpass = input("What is the Telnet/SSH password of the device? ")
     Enablepass = input("What is the enable password of the device? ")
+    timeout = input("What is the privilege timeout of the device? ")
+     if timeout > 0:
+        timeout = f"exec-timeout {timeout}"
+    else:
+        timeout = "0"
     banner = input("What is the banner of the device? ")
-    Delvlan = input("Would you like to delete VLANs on the device? (Y,N) ")
+    passencrpy = input("Do you want Password Encryption enabled? (Y/N) ")
+    if passencrpy == "Y":
+        passencrpy = "service password encryption"
+    else:
+        passencrpy = ""
+    Delvlan = input("Would you like to delete VLANs on the device? (Y,N ) ")
     if Delvlan == "Y":
         vlanStatus = "delete flash:vlan.dat"
     else:
@@ -25,18 +35,23 @@ def Basic(Switch, Router):
             ipv6Status = "ipv6 unicast-routing"
         else:
             print("IPv6 will not be enabled")
+            ipv6 == ""
+            ipv6Status = ""
     Comport = input("What number is the COM port of your serial cable? ")
     Config = F"""
 enable
+{passencrpy}
 clock set {current_time} {dt.day} {now.strftime("%b")} {dt.year}
 conf t
 hostname {hostname}
 enable secret {Enablepass}
 line con 0
 password {Conpass}
+{timeout}
 login
 line vty 0 4
 password {SSHpass}
+{timeout}
 login
 banner motd "{banner}"
 no ip domain-lookup
